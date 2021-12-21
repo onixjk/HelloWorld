51576481e665ec95724de8d7b36d3ac8674f0f2c
onixjk <onixjkk@gmail.ru>
2021-12-21 15:14:50 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Onix Jkk on 21.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
